48188e900285c2181c9d80869625638a6cbcdb6c
Thompson Hall <Quaaysan@gmail.com>
2024-02-11 23:08:41 -0600

    Update fireworks.js
    Adding delay, colors, and fixing bug where emitter changes locations mid explosion

--- Fireworks/fireworks.js
+++ Fireworks/fireworks.js
@@ -1,30 +1,63 @@
 class Akili extends Phaser.Scene {
-    preload(){}
 
-    shoot(pointer){
-        console.log(pointer.x, pointer.y);
-    }
     
     create(){
+        //create texture manager and particle manager
         const graphic = this.make.graphics({x: 400, y: 300, add:false});
-        graphic.fillStyle(0xffffff, 1);
-        graphic.fillCircle(20, 20, 5);
-        graphic.generateTexture('spark', 46, 46);
+        const sparkList = [];
 
-        this.emitter = this.add.particles(400, 400, 'spark', {
-            lifespan: 4000,
-            speed: {min: 150, max: 250},
+        //particle variables
+        const delayAmount = 4000;
+        const particleNumber = 23;
+
+
+        //create list of colors, can edit later
+        const listOfColors = [
+            0x00ff00,
+            0xff0000,
+            0x0000ff,
+            0xffff00,
+            0x00ffff
+        ];
+        
+        //add textures of colors to texture manager with numbers
+        for(const [index,color] of listOfColors.entries()){
+            graphic.clear();
+            graphic.fillStyle(color, 1);
+            graphic.fillCircle(20, 20, 5);
+            graphic.generateTexture('spark'+index.toString(), 46, 46);
+            console.log('spark'+index.toString());
+        }
+        
+        //particle settings
+        const sparkConfig = {
+            lifespan: delayAmount,
+            speed: {min: 10, max: 100},
             scale: {start: 1, end: 0.5},
             gravityY: 100
-        })
+        };
 
+        //timeout function that creates new emitter and deletes it later after set time.
         this.input.on('pointerdown', function(pointer) {
-            this.emitter.explode(16);
-            this.shoot(pointer);
+            if(sparkList.length == 0){ //if there isn't already an emitter in the manager
+                //pick a random number between 0 and length of list
+                const chooseColor = Math.floor(Math.random()*listOfColors.length).toString();
+
+                //add emitter to manager
+                sparkList.push(this.add.particles(pointer.x, pointer.y, 'spark'+chooseColor, sparkConfig));
+
+                //initiate explotion
+                sparkList[0].explode(particleNumber); 
+
+                //start delay to prevent overlap
+                const delay = setTimeout(function (){
+                    sparkList.length = 0;
+                }, delayAmount);
+            };
+
         }, this);
     }
 
-    update(){}
 
 }
 
@@ -47,4 +80,6 @@ okay, so what I want to d o right now is to create something that allows me to d
 2/9/24--
 Okay, finally got around to doing something about this. creating a function that I can change may help me implement later designs
 Also note to self, the game crashes if I define a class without putting anything into it (spent like an hour on this)
+2/11/24--
+Added different colors, delay, and a system for the explosion to happen one the mouse
 */
